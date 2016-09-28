b9e52393bbf65c70e3b5f759b91e203649e8ccdf
Chris Bishop <domingobishop@gmail.com>
2016-09-28 11:35:07 +0100

    bunch of new features

--- functions.php
+++ functions.php
@@ -26,5 +26,300 @@ function tna_child_styles()
     wp_enqueue_style('tna-parent-styles');
     wp_enqueue_style('tna-child-styles');
 }
-
 add_action('wp_enqueue_scripts', 'tna_child_styles');
+
+function tna_child_scripts() {
+    // if ( is_front_page() ) {
+        wp_register_script( 'black-history', get_stylesheet_directory_uri() . '/js/black-history.js', array(),
+            EDD_VERSION, true );
+        wp_register_script( 'equal-heights', get_template_directory_uri() . '/js/jQuery.equalHeights.js', array(),
+            EDD_VERSION, true );
+        wp_register_script( 'equal-heights-var', get_template_directory_uri() . '/js/equalHeights.js', array(),
+            EDD_VERSION, true );
+        wp_register_script( 'moment-js', 'https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.10.3/moment.js', '', '', true );
+        wp_enqueue_script( 'black-history' );
+        wp_enqueue_script( 'equal-heights' );
+        wp_script_add_data( 'equal-heights', 'conditional', 'lte IE 9' );
+        wp_enqueue_script( 'equal-heights-var' );
+        wp_script_add_data( 'equal-heights-var', 'conditional', 'lte IE 9' );
+        wp_enqueue_script( 'moment-js' );
+    // }
+}
+add_action( 'wp_enqueue_scripts', 'tna_child_scripts' );
+
+// Dynamic blog content via RSS feed
+function tna_posts_via_rss( $rss_url, $content_source_name, $number_of_posts, $id ) {
+        // Get feed source
+        $file_headers = @get_headers($rss_url);
+        if ( !$file_headers || $file_headers[0] == 'HTTP/1.1 404 Not Found' ) {
+            $content = false;
+        } else {
+            $content = file_get_contents( $rss_url );
+        }
+        if ( $content !== false ) {
+            $x = new SimpleXmlElement( $content );
+            $n = 0;
+            $html = '';
+
+            // Loop through each feed item and display each item
+            foreach ( $x->channel->item as $item ) {
+                if ( $n == $number_of_posts ) {
+                    break;
+                }
+                if ( isset( $item->enclosure['url'] ) ) {
+                    $enclosure  = $item->enclosure['url'];
+                } else {
+                    $enclosure = 'http://placehold.it/360x180?text=' . $content_source_name;
+                }
+                $namespaces = $item->getNameSpaces( true );
+                $dc         = $item->children( $namespaces['dc'] );
+                $pubDate    = $item->pubDate;
+                $pubDate    = date( "l d M Y", strtotime( $pubDate ) );
+                $description = $item->description;
+                if ( strlen( $description ) > 128 ) {
+                    $description = substr( $description, 0, 128 ) . '...';
+                    if ( substr ( $description, -1, 1 ) != ' ') {
+                        $description = substr( $description, 0, strrpos( $description, " " ) ) . '...';
+                    }
+                }
+                $html .= '<div id="' . $id . '-' . $n . '" class="col-sm-6 col-md-4"><div class="card clearfix">';
+                $html .= '<div class="entry-thumbnail" style="background: url(' . $enclosure . ') no-repeat center center;background-size: cover;">';
+                $html .= '<a href="' . $item->link . '" title="' . $item->title . '">';
+                // $html .= '<img src="' . $enclosure . '" alt="' . $item->title . '" class="hidden">';
+                $html .= '</a></div>';
+                $html .= '<div class="entry-content"><div class="type"><small>' . $content_source_name . '</small></div><h3><a href="' . $item->link . '">';
+                $html .= $item->title;
+                $html .= '</a></h3>';
+                $html .= '<small>' . $dc->creator . ' | ' . $pubDate . '</small>';
+                $html .= '<p>' . $description . '</p></div>';
+                $html .= '</div></div>';
+                $n ++;
+            }
+
+            echo $html;
+        }
+        else {
+            echo '<div class="col-md-4"><div class="card"><div class="entry-content"><h2>Black British History ' . $content_source_name . '</h2><ul class="sibling"><li><a href="' . str_replace( 'feed/', '', $rss_url ) . '">More ' . $content_source_name . '</a></li></ul></div></div></div>';
+        }
+}
+
+// Home page content boxes
+function black_history_meta_boxes() {
+    if (isset($_GET['post'])) {
+        $post_id = $_GET['post'];
+    } else {
+        if (isset($_POST['post_ID'])) {
+            $post_id = $_POST['post_ID'];
+        } else {
+            $post_id = '';
+        }
+    }
+    $template_file = get_post_meta($post_id,'_wp_page_template',TRUE);
+    if ($template_file == 'page-black-history-landing.php') {
+        $meta_boxes[] = array(
+            'id'       => 'bh-first-section',
+            'title'    => 'First content section',
+            'pages'    => 'page',
+            'context'  => 'normal',
+            'priority' => 'high',
+            'fields'   => array(
+                array(
+                    'name' => 'First section heading',
+                    'desc' => 'If the heading field is empty this content section will be disabled',
+                    'id'   => 'first_section_heading',
+                    'type' => 'text',
+                    'std'  => ''
+                ),
+                array(
+                    'name' => 'Title 1',
+                    'desc' => '',
+                    'id'   => 'sec_1_title_1',
+                    'type' => 'text',
+                    'std'  => ''
+                ),
+                array(
+                    'name' => 'Link 1',
+                    'desc' => '',
+                    'id'   => 'sec_1_link_1',
+                    'type' => 'text',
+                    'std'  => ''
+                ),
+                array(
+                    'name' => 'Image URL 1',
+                    'desc' => '',
+                    'id'   => 'sec_1_image_1',
+                    'type' => 'text',
+                    'std'  => ''
+                ),
+                array(
+                    'name' => 'Short description 1',
+                    'desc' => '',
+                    'id'   => 'sec_1_description_1',
+                    'type' => 'textarea',
+                    'std'  => ''
+                ),
+                array(
+                    'name' => 'Title 2',
+                    'desc' => '',
+                    'id'   => 'sec_1_title_2',
+                    'type' => 'text',
+                    'std'  => ''
+                ),
+                array(
+                    'name' => 'Link 2',
+                    'desc' => '',
+                    'id'   => 'sec_1_link_2',
+                    'type' => 'text',
+                    'std'  => ''
+                ),
+                array(
+                    'name' => 'Image URL 2',
+                    'desc' => '',
+                    'id'   => 'sec_1_image_2',
+                    'type' => 'text',
+                    'std'  => ''
+                ),
+                array(
+                    'name' => 'Short description 2',
+                    'desc' => '',
+                    'id'   => 'sec_1_description_2',
+                    'type' => 'textarea',
+                    'std'  => ''
+                ),
+                array(
+                    'name' => 'Title 3',
+                    'desc' => '',
+                    'id'   => 'sec_1_title_3',
+                    'type' => 'text',
+                    'std'  => ''
+                ),
+                array(
+                    'name' => 'Link 3',
+                    'desc' => '',
+                    'id'   => 'sec_1_link_3',
+                    'type' => 'text',
+                    'std'  => ''
+                ),
+                array(
+                    'name' => 'Image URL 3',
+                    'desc' => '',
+                    'id'   => 'sec_1_image_3',
+                    'type' => 'text',
+                    'std'  => ''
+                ),
+                array(
+                    'name' => 'Short description 3',
+                    'desc' => '',
+                    'id'   => 'sec_1_description_3',
+                    'type' => 'textarea',
+                    'std'  => ''
+                )
+            )
+        );
+        $meta_boxes[] = array(
+            'id'       => 'bh-second-section',
+            'title'    => 'Second content section',
+            'pages'    => 'page',
+            'context'  => 'normal',
+            'priority' => 'high',
+            'fields'   => array(
+                array(
+                    'name' => 'Display section content boxes',
+                    'desc' => 'Select &#39;disabled&#39; to hide these boxes',
+                    'id' => 'second_section_display',
+                    'type' => 'select',
+                    'options' => array('disabled', 'enabled')
+                ),
+                array(
+                    'name' => 'Title 1',
+                    'desc' => '',
+                    'id'   => 'content_title_1',
+                    'type' => 'text',
+                    'std'  => ''
+                ),
+                array(
+                    'name' => 'Sub title 1',
+                    'desc' => '',
+                    'id'   => 'content_sub_title_1',
+                    'type' => 'text',
+                    'std'  => ''
+                ),
+                array(
+                    'name' => 'Image URL 1',
+                    'desc' => '',
+                    'id'   => 'content_image_1',
+                    'type' => 'text',
+                    'std'  => ''
+                ),
+                array(
+                    'name' => 'Content 1',
+                    'desc' => '',
+                    'id'   => 'content_1',
+                    'type' => 'textarea',
+                    'std'  => ''
+                ),
+                array(
+                    'name' => 'Title 2',
+                    'desc' => '',
+                    'id'   => 'content_title_2',
+                    'type' => 'text',
+                    'std'  => ''
+                ),
+                array(
+                    'name' => 'Sub title 2',
+                    'desc' => '',
+                    'id'   => 'content_sub_title_2',
+                    'type' => 'text',
+                    'std'  => ''
+                ),
+                array(
+                    'name' => 'Image URL 2',
+                    'desc' => '',
+                    'id'   => 'content_image_2',
+                    'type' => 'text',
+                    'std'  => ''
+                ),
+                array(
+                    'name' => 'Content 2',
+                    'desc' => '',
+                    'id'   => 'content_2',
+                    'type' => 'textarea',
+                    'std'  => ''
+                ),
+                array(
+                    'name' => 'Title 3',
+                    'desc' => '',
+                    'id'   => 'content_title_3',
+                    'type' => 'text',
+                    'std'  => ''
+                ),
+                array(
+                    'name' => 'Sub title 3',
+                    'desc' => '',
+                    'id'   => 'content_sub_title_3',
+                    'type' => 'text',
+                    'std'  => ''
+                ),
+                array(
+                    'name' => 'Image URL 3',
+                    'desc' => '',
+                    'id'   => 'content_image_3',
+                    'type' => 'text',
+                    'std'  => ''
+                ),
+                array(
+                    'name' => 'Content 3',
+                    'desc' => '',
+                    'id'   => 'content_3',
+                    'type' => 'textarea',
+                    'std'  => ''
+                )
+            )
+        );
+        // Adds meta box to page
+        foreach ( $meta_boxes as $meta_box ) {
+            $form_box = new CreateMetaBox( $meta_box );
+        }
+    }
+}
+add_action( 'init', 'black_history_meta_boxes' );
--- page-black-history-landing.php
+++ page-black-history-landing.php
@@ -4,55 +4,201 @@ Template Name: Black History landing
 */
 get_header(); ?>
 <div class="black-history">
+	<?php while ( have_posts() ) : the_post();
+	$feature_img = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'full' );
+	if ( $feature_img ) { ?>
+	<div class="banner feature-img" role="banner" style="background: url( <?php echo $feature_img[0] ?> ) no-repeat center center;background-size: cover;">
+	<?php } else { ?>
 	<div class="banner" role="banner">
+	<?php } ?>
 		<?php get_template_part( 'breadcrumb' ); ?>
 		<div class="heading-banner text-center">
 			<div class="container">
 				<div class="row">
 					<div class="col-md-12">
-						<h1>Black History</h1>
+						<h1><?php the_title(); ?></h1>
 					</div>
 				</div>
+				<?php get_image_caption( 'top' ); ?>
 			</div>
 		</div>
 	</div>
-</div>
-<div class="menu" role="navigation">
-	<div class="container">
-		<div class="row">
-			<nav class="col-md-12 text-center">
-				<ul>
-					<li>
-						<a href="#">Menu item 1</a>
-					</li>
-					<li>
-						<a href="#">Menu item 2</a>
-					</li>
-					<li>
-						<a href="#">Menu item 3</a>
-					</li>
-				</ul>
-			</nav>
+	<div class="menu" role="navigation">
+		<div class="container">
+			<div class="row">
+				<nav class="col-md-12 text-center">
+					<ul>
+						<li>
+							<a href="#blog-0">From our blog</a>
+						</li>
+						<li>
+							<a href="#event">What's on</a>
+						</li>
+						<li>
+							<a href="#podcast-0">Podcasts</a>
+						</li>
+						<?php
+							$first_section_heading = get_post_meta( $post->ID, 'first_section_heading', true );
+							if ( $first_section_heading ) { ?>
+						<li>
+							<a href="#first-content-section"><?php echo $first_section_heading ?></a>
+						</li>
+							<?php } ?>
+					</ul>
+				</nav>
+			</div>
 		</div>
 	</div>
-</div>
-<main id="main" class="content-area" role="main">
-	<div class="container">
-		<div class="row">
-			<section>
-				<div class="col-md-12">
-					<article>
-						<div class="entry-header">
-							<h2>Black History</h2>
+	<main id="main" role="main" class="content-area">
+		<div class="container">
+			<?php if ( get_post()->post_content ) { ?>
+			<section id="page-content">
+				<div class="row">
+					<div class="col-md-12">
+						<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+							<div class="entry-content clearfix">
+								<?php the_content(); ?>
+							</div>
+						</article>
+					</div>
+				</div>
+			</section>
+			<?php } ?>
+			<section id="dynamic-content">
+				<div class="row">
+					<div class="equal-heights equal-heights-flex-box">
+						<?php tna_posts_via_rss( 'http://blog.nationalarchives.gov.uk/blog/tag/black-british-history-portal/feed/', 'Blog', 3, 'blog' ) ?>
+						<div class="col-sm-6 col-md-4">
+							<div id="event" class="card event clearfix">
+								<div class="entry-thumbnail">
+									<a href="http://nationalarchives.eventbrite.co.uk/" title="The National Archives events" target="_blank">
+										<img src="<?php echo make_path_relative( get_stylesheet_directory_uri() ) ?>/img/event.jpg" alt="Black British History Event">
+									</a>
+								</div>
+								<div class="entry-content">
+									<div class="type">
+										<small>What&prime;s on</small>
+									</div>
+									<h3>Black British History events</h3>
+									<p>
+										<i>Events programme loading.</i><br>
+										If it does not appear after 10 seconds please
+										<a href="http://nationalarchives.eventbrite.co.uk/" title="The National Archives events" target="_blank">click here</a>.
+									</p>
+								</div>
+							</div>
 						</div>
-						<div class="entry-content clearfix">
-							<p>content</p>
+						<?php tna_posts_via_rss( 'http://media.nationalarchives.gov.uk/index.php/tag/black-british-history/feed/', 'Podcast', 2, 'podcast' ) ?>
+					</div>
+				</div>
+			</section>
+			<?php if ( $first_section_heading ) { ?>
+			<section id="first-content-section">
+				<div class="row">
+					<div class="col-md-12">
+						<article>
+							<div class="entry-header">
+								<h2><?php echo $first_section_heading ?></h2>
+							</div>
+							<div class="entry-content clearfix">
+								<?php
+									for ($i = 1; $i <= 3; $i++) {
+										$title = get_post_meta( $post->ID, 'sec_1_title_'.$i, true );
+										$link = get_post_meta( $post->ID, 'sec_1_link_'.$i, true );
+										$image = get_post_meta( $post->ID, 'sec_1_image_'.$i, true );
+										$description = get_post_meta( $post->ID, 'sec_1_description_'.$i, true );
+										if ( $title ) { ?>
+											<div class="col-md-4">
+												<div class="card">
+													<div class="entry-thumbnail">
+														<a href="<?php echo $link ?>" title="<?php echo $title ?>">
+															<img src="<?php echo make_path_relative( $image ) ?>" alt="<?php echo $title ?>">
+														</a>
+													</div>
+													<div class="entry-content">
+														<h3>
+															<a href="<?php echo $link ?>" title="<?php echo $title ?>">
+																<?php echo $title ?>
+															</a>
+														</h3>
+														<p><?php echo $description ?></p>
+													</div>
+												</div>
+											</div>
+										<?php }
+									}
+								?>
+							</div>
+						</article>
+					</div>
+				</div>
+			</section>
+			<?php } ?>
+			<?php if ( get_post_meta( $post->ID, 'second_section_display', true ) == 'enabled' ) { ?>
+			<section id="second-content-section">
+				<div class="row">
+					<div class="equal-heights equal-heights-flex-box">
+						<?php
+						for ($i = 1; $i <= 3; $i++) {
+							$title = get_post_meta( $post->ID, 'content_title_'.$i, true );
+							$sub_title = get_post_meta( $post->ID, 'content_sub_title_'.$i, true );
+							$image = get_post_meta( $post->ID, 'content_image_'.$i, true );
+							$content = get_post_meta( $post->ID, 'content_'.$i, true );
+							if ( $title ) { ?>
+								<div class="col-xs-12 col-sm-12 col-md-4">
+									<article class="card">
+										<div class="entry-header">
+											<h2><?php echo $title ?></h2>
+										</div>
+										<div class="entry-content clearfix">
+											<div class="entry-thumbnail">
+												<?php if ( $image ) { ?>
+													<img src="<?php echo make_path_relative( $image ) ?>" class="img-responsive" alt="<?php echo $title ?>">
+												<?php } else { ?>
+													<img src="<?php echo make_path_relative( get_stylesheet_directory_uri() ) ?>/img/event-list.jpg" class="img-responsive" alt="<?php echo $title ?>">
+												<?php } ?>
+											</div>
+											<h3><?php echo $sub_title ?></h3>
+											<p><?php echo $content ?></p>
+										</div>
+									</article>
+								</div>
+							<?php }
+						}
+						?>
+					</div>
+			</section>
+			<?php } ?>
+			<section id="connect">
+				<div class="connect">
+					<div class="row">
+						<div class="col-sm-6">
+							<h2>Stay up-to-date</h2>
 						</div>
-					</article>
+						<div class="col-sm-4">
+							<form name="signup" id="banner-form" class="pad-medium" action="http://dmtrk.co.uk/signup.ashx" method="post" role="form">
+								<input type="hidden" name="addressbookid" value="281378">
+								<input type="hidden" name="userid" value="28895">
+								<input type="hidden" name="ReturnURL" value="http://nationalarchives.gov.uk/news/subscribe-confirmation.htm">
+								<label class="sr-only" for="email">Enter your email address to subscribe to our newsletter</label>
+								<input type="email" id="email" class="email" name="Email" required="" placeholder="Enter your email address" aria-label="Enter your email address to subscribe to our newsletter" aria-required="true">
+								<input id="newsletterSignUp" type="submit" value="Subscribe" class="button">
+							</form>
+						</div>
+						<div class="col-sm-2">
+							<a href="http://www.facebook.com/TheNationalArchives" target="_blank" title="External website Facebook - link opens in a new window">
+								<span class="sprite icon-facebook"></span>
+							</a>
+							<a href="https://twitter.com/UkNatArchives" target="_blank" title="External website Twitter - link opens in a new window">
+								<span class="sprite icon-twitter"></span>
+							</a>
+						</div>
+					</div>
 				</div>
 			</section>
 		</div>
-	</div>
-</main>
+	</main>
+	<?php endwhile; ?>
+</div>
 <?php get_footer(); ?>
 
