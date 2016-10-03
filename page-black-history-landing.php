d4f1c3b2c97f51d691e9b8012a6dab556927539c
Chris Bishop <domingobishop@gmail.com>
2016-10-03 12:27:15 +0100

    new newsletter signup form

--- page-black-history-landing.php
+++ page-black-history-landing.php
@@ -176,13 +176,13 @@ get_header(); ?>
 							<h2>Stay up-to-date</h2>
 						</div>
 						<div class="col-sm-4">
-							<form name="signup" id="banner-form" class="pad-medium" action="http://dmtrk.co.uk/signup.ashx" method="post" role="form">
-								<input type="hidden" name="addressbookid" value="281378">
-								<input type="hidden" name="userid" value="28895">
+							<form name="signup" id="signup" class="pad-medium" action="http://r1.wiredemail.net/signup.ashx" method="post" role="form">
+								<input type="hidden" name="addressbookid" value="636353"> <!-- homepage and general sign up -->
+								<input type="hidden" name="userid" value="173459">
 								<input type="hidden" name="ReturnURL" value="http://nationalarchives.gov.uk/news/subscribe-confirmation.htm">
 								<label class="sr-only" for="email">Enter your email address to subscribe to our newsletter</label>
-								<input type="email" id="email" class="email" name="Email" required="" placeholder="Enter your email address" aria-label="Enter your email address to subscribe to our newsletter" aria-required="true">
-								<input id="newsletterSignUp" type="submit" value="Subscribe" class="button">
+								<input type="email" id="email" class="email" name="Email" required="required" placeholder="Enter your email address" aria-label="Enter your email address to subscribe to our newsletter" aria-required="true">
+								<input id="newsletterSignUp" type="submit" name="Submit" value="Subscribe" class="button">
 							</form>
 						</div>
 						<div class="col-sm-2">
